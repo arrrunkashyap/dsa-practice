f94d3670f6d8c37787ded25705cf6e7cd8ae7857
Arun Kumar <actionboy7900@gmail.com>
2026-07-29 20:56:05 +0530

    Implement palindrome checking in solution.cpp

---
+++ 0125-valid-palindrome/solution.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+class Solution
+{
+	public:
+	bool isPalindrome(string s)
+	{
+		int low = 0;
+        int high = s.size() - 1;
+
+		while (low < high)
+		{
+			if (isalnum(s[low]))
+			{
+				if (isalnum(s[high]))
+				{
+					if (tolower(s[low]) != tolower(s[high]))
+					{
+						return false;
+					}
+					low++;
+				}
+				high--;
+			}
+			else
+			{
+				if (!isalnum(s[high]))
+				{
+					high--;
+				}
+				low++;
+			}
+		}
+		return true;
+	}
+};
